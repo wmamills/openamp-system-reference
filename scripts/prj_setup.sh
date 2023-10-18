422ee46aaefb410af5799559abd4e9afd9d5112b
Bill Mills <bill.mills@linaro.org>
2023-10-18 19:40:45 -0400

    prj_setup.sh: Split into setup and build phases
    Do all setup in separate functions.
    This allows us to do the setup in the container build and keep the build
    times short and less verbose.
    
    The build functions should not use sudo at all.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- scripts/prj_setup.sh
+++ scripts/prj_setup.sh
@@ -15,37 +15,44 @@ ZEPHYR_SDK_DOWNLOAD_URL=$ZEPHYR_SDK_DOWNLOAD_FOLDER/$ZEPHYR_SDK_SETUP_TAR
 
 FREERTOS_ZIP_URL=https://cfhcable.dl.sourceforge.net/project/freertos/FreeRTOS/V10.0.1/FreeRTOSv10.0.1.zip
 
-pre_build(){
+setup_common() {
 	apt update
 	apt-get install -y make
 	sudo pip3 install cmake
 }
 
-build_linux(){
-	echo  " Build for linux"
-	apt-get install -y libsysfs-dev libhugetlbfs-dev gcc
+setup_linux() {
+	echo " Setup for linux"
+	sudo apt-get install -y libsysfs-dev libhugetlbfs-dev gcc
+}
+
+build_linux() {
 	mkdir -p build-linux
 	cd build-linux
 	cmake .. -DWITH_TESTS_EXEC=on
 	make VERBOSE=1 all test
-	exit 0
 }
 
-build_generic(){
-	echo  " Build for generic platform "
-	apt-get install -y gcc-arm-none-eabi
+setup_generic() {
+	echo " Setup for generic platform "
+	sudo apt-get install -y gcc-arm-none-eabi
+}
+
+build_generic() {
 	mkdir -p build-generic
 	cd build-generic
 	cmake .. -DCMAKE_TOOLCHAIN_FILE=template-generic
 	make VERBOSE=1
-	exit 0
 }
 
-build_freertos(){
-	echo  " Build for freertos OS "
-	apt-get install -y gcc-arm-none-eabi unzip
+setup_freertos() {
+	echo  " Setup for freertos OS "
+	sudo apt-get install -y gcc-arm-none-eabi unzip
 	wget $FREERTOS_ZIP_URL --dot-style=giga > /dev/null
 	unzip FreeRTOSv10.0.1.zip > /dev/null
+}
+
+build_freertos() {
 	mkdir -p build-freertos
 	cd build-freertos export
 	cmake .. -DCMAKE_TOOLCHAIN_FILE=template-freertos \
@@ -53,11 +60,12 @@ build_freertos(){
 		-I$PWD/../FreeRTOSv10.0.1/FreeRTOS/Demo/CORTEX_STM32F107_GCC_Rowley \
 		-I$PWD/../FreeRTOSv10.0.1/FreeRTOS/Source/portable/GCC/ARM_CM3"
 	make VERBOSE=1
-	exit 0
 }
 
-build_zephyr(){
-	echo  " Build for Zephyr OS "
+setup_zephyr() {
+	echo  " Setup for Zephyr OS "
+
+	# install needed packages from OS
 	sudo apt-get install -y git cmake ninja-build gperf
 	sudo apt-get install -y ccache dfu-util device-tree-compiler wget pv
 	sudo apt-get install -y python3-dev python3-pip python3-setuptools python3-tk \
@@ -65,42 +73,99 @@ build_zephyr(){
 	sudo apt-get install -y make gcc gcc-multilib g++-multilib libsdl2-dev
 	sudo apt-get install -y libc6-dev-i386 gperf g++ python3-ply python3-yaml \
 		device-tree-compiler ncurses-dev uglifyjs -qq
+
+	# Install things from python pip
 	sudo pip3 install pyelftools
 	pip3 install --user -U west
 	echo 'export PATH=~/.local/bin:"$PATH"' >> ~/.bashrc
 	source ~/.bashrc
 
+	# Install the zephyr SDK
 	wget $ZEPHYR_SDK_DOWNLOAD_URL --dot-style=giga
 	echo "Extracting $ZEPHYR_SDK_SETUP_TAR"
 	pv $ZEPHYR_SDK_SETUP_TAR -i 3 -ptebr -f | tar xJ
 	rm -rf $ZEPHYR_SDK_INSTALL_DIR
 	yes | ./$ZEPHYR_SDK_SETUP_DIR/setup.sh
-	cd top
-	west init -l ./openamp-system-reference
+
+	# The users should be in the dir they want for the top level
+	# Pre-clone the whole Zephyr workspace
+	# It is fine that we pre-clone main as the versions will get fixed up
+	# by the real manifest
+	west init
 	west update --narrow
 	west zephyr-export
+
+	# now remove the manifest so we can supply our own in the build
+	# use a bit of a sanity test be we do this
+	if [ -d zephyr ]; then
+		rm -rf .west
+	fi
+
+	# Install python packages zephyr needs
+	pip3 install --user -r ./zephyr/scripts/requirements.txt
+}
+
+build_zephyr() {
+	echo  " Build for Zephyr OS "
+
+	# The users should be in the top dir
+	# Clone the whole Zephyr workspace
+	west init -l ./openamp-system-reference
+	west update --narrow
+
+	# Recheck python packages incase they have changed
 	pip3 install --user -r ./zephyr/scripts/requirements.txt
 
 	west build -p -b stm32mp157c_dk2 openamp-system-reference/examples/zephyr/rpmsg_multi_services
 	west build -p -b kv260_r5        openamp-system-reference/examples/zephyr/rpmsg_multi_services
-	exit 0
 }
 
-main(){
-	pre_build;
-
-	if [[ "$TARGET" == "linux" ]]; then
+main() {
+	case "$TARGET" in
+	setup_linux)
+		setup_common
+		setup_linux
+		;;
+	setup_generic)
+		setup_common
+		setup_generic
+		;;
+	setup_freertos)
+		setup_common
+		setup_freertos
+		;;
+	setup_zephyr)
+		setup_common
+		setup_zephyr
+		;;
+	setup_all)
+		setup_common
+		setup_linux
+		setup_generic
+		setup_freertos
+		setup_zephyr
+		;;
+	linux)
 		build_linux
-	fi
-	if [[ "$TARGET" == "generic" ]]; then
+		;;
+	generic)
 		build_generic
-	fi
-	if [[ "$TARGET" == "freertos" ]]; then
+		;;
+	freertos)
 		build_freertos
-	fi
-	if [[ "$TARGET" == "zephyr" ]]; then
+		;;
+	zephyr)
 		build_zephyr
-	fi
+		;;
+	all)
+		build_linux
+		build_generic
+		build_freertos
+		build_zephyr
+		;;
+	*)
+		echo "Unknown target $TARGET" && false
+	esac
 }
 
 main
